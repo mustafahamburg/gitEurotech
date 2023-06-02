13533da4aaa8d2753881724070b208430b5421b0
mustafa <mstfozturk25hh@gmail.com>
2023-06-02 20:50:40 +0200

    mustafa1 added

--- src/test/java/LoginTests/mustafa1.java
+++ src/test/java/LoginTests/mustafa1.java
@@ -4,5 +4,9 @@ public class mustafa1 {
     public static void main(String[] args) {
         String name="Tarkan";
         String song= "Uyan";
+        //***************************
+
+        String country="Deutschland";
+        String city="Hamburg";
     }
 }
